d86d02bc53674a2a228477064ea4304707e6ca4a
adi <adig15221@gmail.com>
2025-04-21 10:37:22 +0530

    Add files via upload

---
+++ command_demo2.java
@@ -0,0 +1,14 @@
+class command_demo2
+{
+	public static void main(String args[])
+	{
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int sum=0;
+		sum=a+b;
+		{
+			System.out.println("sum="+sum);
+		}
+	}
+}
